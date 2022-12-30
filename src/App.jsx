67de592e0c7c4dd9b33bb05910520f5d8e1fcca6
Bastián Núñez <bastinuez@hotmail.com>
2022-12-30 13:08:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TaskList from './components/TaskList'
+import TaskForm from './components/TaskForm'
+
+function App() {
+
+  return (
+    <main className='bg-zinc-900 h-screen p-10'>
+      <div className='container mx-auto'>
+        <TaskForm/>
+        <TaskList/>
+      </div>
+      
+    </main>
+  )
+}
+
+export default App
